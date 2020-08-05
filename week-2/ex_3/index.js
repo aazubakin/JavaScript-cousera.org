17b768aff381ba316aff54233f1e1e04d50b417a
Aleksandr Zubakin <eldoson@yandex.ru>
2020-08-05 14:12:22 +0500

    Final solution

--- week-2/ex_3/index.js
+++ week-2/ex_3/index.js
@@ -1,5 +1,8 @@
 // Телефонная книга
 var phoneBook = {};
+var mySet = new Set();
+phoneBook = mySet;
+//console.log(commandName);
 
 /**
  * @param {String} command
@@ -27,71 +30,111 @@ var phoneBook = {};
    ***
    Команда SHOW
    Возвращает массив контактов с их телефонами. Массив содержит строчки вида: "Имя: Телефон1, Телефон2". Массив должен быть отсортирован по имени контакта. Телефоны идут в порядке добавления их в телефонную книгу. Контакт с пустым списком телефонов не должен возвращаться.
+var phoneBook = require('./index.js');
 
+phoneBook('ADD Ivan 555-10-01,555-10-03');
+
+phoneBook('ADD Ivan 555-10-02');
+
+console.info(phoneBook('SHOW'));
+
+// Вывод:
+
+// ["Ivan: 555-10-01, 555-10-03, 555-10-02"]
+
+phoneBook('REMOVE_PHONE 555-10-03');
+
+phoneBook('ADD Alex 555-20-01');
+
+phoneBook('SHOW');
+
+// Вывод:
+
+// ["Alex: 555-20-01", "Ivan: 555-10-01, 555-10-02"]
+
+phoneBook('REMOVE_PHONE 555-20-01');
+
+phoneBook('SHOW');
+
+// Вывод:
+
+// ["Ivan: 555-10-01, 555-10-02"]
  */
 module.exports = function (command) {
    // ...
-   var comandArr = command.split(/ |, /);
+   var comandArr = command.split(/ |,|, /);
    var commandName = comandArr[0];
+   comandArr.shift();
    var result = [];
    var removed = false;
-   //console.log(commandName);
+   var res = [];
    // ...
    // Обработка команды ADD
    function addPhone() {
-      for (let i = 1; i < comandArr.length - 1; i++) {
-         if (comandArr[1] in phoneBook) {
-            phoneBook[comandArr[1]] += ', ' + comandArr[i + 1];
-         } else {
-            phoneBook[comandArr[1]] = comandArr[2];
-         }
+      for (var i = 0; i < comandArr.length; i++) {
+         if (i === 0) {
+            phoneBook.add(comandArr[i] + ':');
+         } else phoneBook.add(comandArr[i]);
       }
-      result.push(phoneBook[comandArr[1]]);
    }
-   // Продолжить здесь -----------------------------------------------------------------------
+   //Обработка команды REMOVE 
    function removePhone() {
-      let index = 0;
-      for (let key in phoneBook) {
-         index = phoneBook[key].indexOf(comandArr[1]);
-         if (index || index === 0) {
-            phoneBook[key] = phoneBook[key].replace(phoneBook[key].slice(index, index + 11), '');
+      for (var i = 0; i < comandArr.length; i++) { //возможность удаления нескольких телефонов за раз
+         if (phoneBook.has(comandArr[i])) {
+            phoneBook.delete(comandArr[i]);
             removed = true;
          }
       }
    }
-
+   //Обработка команды SHOW
    function showItem() {
-      let show;
-      for (let key in phoneBook) {
-         show = key + ': ' + phoneBook[key];
-         result.push(show);
+      var resNames = [];
+      var resNumbers = [];
+      //получаем имена 
+      for (var i = 0; i < result.length; i++) {
+         if (result[i].search(/[a-z]/) !== -1) {
+            resNames.push(i);
+         }
       }
+      //получаем номера телефонов
+      for (i = 0; i <= resNames.length - 1; i++) {
+         resNumbers.push(result.slice(resNames[i] + 1, resNames[i + 1]).join(', '));
+      }
+      //соединяем имя с группой телефонов
+      for (i = resNumbers.length - 1; i != -1; i--) {
+         //не выводить имени если номер удален
+         if (resNumbers[i] == '') {
+            continue;
+         }
+         res.push(result[resNames[i]] + ' ' + resNumbers[i]);
+      }
+      //console.log(phoneBook);
    }
 
    if (commandName === 'ADD') {
       // ...
       addPhone();
-      console.log(result);
-      // ...Обработка других команд... 
-      return result;
+      //return result;
    }
    if (commandName === 'REMOVE_PHONE') {
       removePhone();
       return removed;
    }
    if (commandName === 'SHOW') {
-      //showItem();
-      console.log(result);
-      return result;
+      result = [...phoneBook];
+      showItem();
+      //console.log(res);
+      return res;
    }
-
 };
 
 
-
-module.exports('ADD Ivan 555-10-01, 555-10-03');
-//module.exports('SHOW');
-//   module.exports('ADD Alex 555-10-02');
- //  module.exports('SHOW');
-//   module.exports('REMOVE_PHONE 555-10-03');
-//      module.exports('SHOW'); 
+/*
+module.exports('ADD Ivan 555-10-01,555-10-03');
+module.exports('ADD Ivan 555-10-02');
+module.exports('SHOW');
+module.exports('REMOVE_PHONE 555-10-03');
+module.exports('ADD Alex 555-20-01');
+module.exports('SHOW');
+module.exports('REMOVE_PHONE 555-20-01');
+module.exports('SHOW'); */
